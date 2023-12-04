5fe83874c9107171a1d47405842ff37e50c44c8b
Vincent Flores <vincentflores88@gmail.com>
2023-12-04 17:42:23 +0800

    updates

--- book-learning/chapter9/cstring.cpp
+++ book-learning/chapter9/cstring.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 int main()
 {
     // char name3[];
     char name[] = "John";
     string name2 = "John";
-    cout << name ;
+    // int length = strlen(name);
+    int length = name2.size();
+    
+    cout << name2.size() ;
 }
