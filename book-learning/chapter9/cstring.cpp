cb54893e59a5c03121ce0c09c923a098d7891712
Vincent Flores <vincentflores88@gmail.com>
2023-12-11 15:56:55 +0800

    updates

--- book-learning/chapter9/cstring.cpp
+++ book-learning/chapter9/cstring.cpp
@@ -7,7 +7,11 @@ int main()
     // char name3[];
     char name[] = "John";
     string name2 = "John";
+    string str="We think in generalities, but we live in details.";
     // int length = strlen(name);
+    size_t pos = str.find("live");
+    string str3 = str.substr (pos); 
+    string str4 = name2.substr (1); 
     int length = name2.size();
     
     cout << name2.size() ;
